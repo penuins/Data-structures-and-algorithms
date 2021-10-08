3f8de6f98a348cc3afadd923492a84a9206ec6c4
penu.c <87123013+penuins@users.noreply.github.com>
2021-10-08 21:08:52 +0800

    Update SqList_Problems.h

--- 线性表/SqList_Problems.h
+++ 线性表/SqList_Problems.h
@@ -1,11 +1,4 @@
-#include"SqList.h"
-#include<vector>
-
-//
-// 1. 旋转数组
-//
-
-// 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数
+/* -----旋转数组----- */
 void Rotate(SqList& L, int k)
 {
 	k = k % L.length;                   // 循环右移位数
@@ -14,7 +7,7 @@ void Rotate(SqList& L, int k)
 	Reverse(L, k - 1, L.length - 1);    // b^(-1)a -> b^(-1)a^(-1)
 }
 
-// 将数组中从 left 到 right 的部分逆置
+/* 将数组中从 left 到 right 的部分逆置 */
 void Reverse(SqList& L, int left, int right)
 {
 	while(left < right)
@@ -25,7 +18,7 @@ void Reverse(SqList& L, int left, int right)
 	}
 }
 
-// 交换 a 与 b 的值
+/* 交换 a 与 b 的值 */
 void swap(int& a, int& b)
 {
 	int temp = a;
@@ -35,14 +28,9 @@ void swap(int& a, int& b)
 
 
 
-//
-// /* 双指针法 */ //
-//
+/* -----双指针法----- */
 
-//
-// 2. 删除排序数组中值为 x 的元素
-//
-
+/* -----删除排序数组中值为 x 的元素----- */
 void Del_x(SqList& L, int x)
 {
 	int fast = 0;            // fast: 遍历数组到达的下标位置
@@ -56,10 +44,7 @@ void Del_x(SqList& L, int x)
 	L.length = slow + 1;     // 更新顺序表的长度
 }
 
-//
-// 3. 删除排序数组中重复的元素
-//
-
+/* -----删除排序数组中重复的元素----- */
 void Remove_Duplicates(SqList& L)
 {
 	int fast = 1;           // 快指针 fast：遍历数组到达的下标位置
@@ -73,3 +58,101 @@ void Remove_Duplicates(SqList& L)
 	L.length = slow;        // 更新顺序表的长度
 }
 
+/* -----合并有序顺序表----- */
+bool MergeList(SqList A, SqList B, SqList& C)
+{
+	if(A.length + B.length > MaxSize)    // 大于顺序表的最大长度，返回 false
+		return false;
+	int i = 0, j = 0, k = 0;
+	while(i < A.length && j < B.length)  // 循环，两两比较，较小者存入 C
+	{
+		if(A.data[i] <= B.data[j])
+			C.data[k++] = A.data[i++];
+		else
+			C.data[k++] = B.data[j++];
+	}
+	// 将剩余顺序表
+	while(i < A.length)
+		C.data[k++] = A.data[i++];
+	while(j < B.length)
+		C.data[k++] = B.data[j++];
+	C.length = k;
+	return true;
+}
+
+/* 两升序顺序表的中位数 */
+int M_Search(int A[], int B[], int n)
+{
+	int left1 = 0, right1 = n - 1, mid1;
+	int left2 = 0, right2 = n - 1, mid2;
+	while(left1 != right1 || left2 != right2)
+	{
+		mid1 = (left1 + right1) / 2;
+		mid2 = (left2 + right2) / 2;
+		// a == b
+		if(A[mid1] == B[mid2])
+			return A[mid1];
+		// a < b 舍弃序列 A 中较小一半，舍弃序列 B 中较大的一半，两次舍弃的长度相等
+		if(A[mid1] < B[mid2])
+		{
+			if((left1 + right1) % 2 == 0)
+			{
+				left1 = mid1;
+				right2 = mid2;
+			}
+			else
+			{
+				left1 = mid1 + 1;
+				right2 = mid2;
+			}
+		}
+		// a > b 舍弃序列 A 中较大一半，舍弃序列 B 中较小的一半，两次舍弃的长度相等
+		else
+		{
+			if((left2 + right2) % 2 == 0)
+			{
+				right1 = mid1;
+				right2 = mid2;
+			}
+			else
+			{
+				right1 = mid1;
+				right2 = mid2 + 1;
+			}
+		}
+	}
+	// 直到两序列中都只剩一个元素，则较小者为所求的中位数
+	if(A[left1] < B[left2])
+		return A[left1];
+	else
+		return B[left2];
+}
+
+int Majority(int A[], int n)
+{
+    int i = 0, major, count = 0;
+    while(i < n)
+    {
+        if(count == 0)
+        {
+            major = A[i];
+            count++;
+        }
+        else
+        {
+            if(A[i] == major)
+                count++;
+            else
+                count--;
+        }
+        i++;
+    }
+    if(count > 0)
+        for(i = count = 0; i < n; i++)
+            if(major == A[i])
+                count++;
+    if(count > n / 2)
+        return major;
+    else
+        return -1;
+}
