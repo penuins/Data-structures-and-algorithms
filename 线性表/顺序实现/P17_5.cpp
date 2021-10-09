c0ef6408362f640f6606a3999523faf35a3c4c07
penu.c <87123013+penuins@users.noreply.github.com>
2021-10-09 23:08:32 +0800

    Create P17_5.cpp

---
+++ 线性表/顺序实现/P17_5.cpp
@@ -0,0 +1,22 @@
+//
+// 从顺序表中删除值在s和t之间的元素，其中s<t
+//
+
+bool Del_s_t(SqList& L, int s, int t)
+{
+    // 参数合法性检查
+    if(s >= t)
+        return false;
+    
+    // 双指针法
+    int slow = fast = 0;
+    while(fast < L.length)
+    {
+        if(L.data[fast] < s || L.data[fast] > t)
+            L.data[slow++] = L.data[fast];
+        fast++;
+    }
+    
+    // 更新顺序表长度
+    L.length = slow + 1;
+}
