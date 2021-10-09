70dada84dfe16879a26450efa8f0374a3c24481a
penu.c <87123013+penuins@users.noreply.github.com>
2021-10-09 22:44:02 +0800

    Create P17_3.cpp

---
+++ 线性表/顺序实现/P17_3.cpp
@@ -0,0 +1,19 @@
+//
+// 删除线性表中所有值为x的元素
+//
+
+void Del_x(SqList& L, ElemType x)
+{
+    // 双指针法
+    // slow：下一个元素应当插入的数组下标
+    // fast：正在遍历的数组下标
+    int slow = fast = 0;
+    while(fast < L.length)
+    {
+        if(L.data[fast] != x)
+            L.data[slow++] = L.data[fast];
+        fast++;
+    }
+    // 更新顺序表表长
+    L.length = slow + 1;
+}
