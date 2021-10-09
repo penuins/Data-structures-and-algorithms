e010df8d3d6d140cae50a18d4e5c2ac4b1b71ec6
penu.c <87123013+penuins@users.noreply.github.com>
2021-10-09 22:32:49 +0800

    Create P19_1.cpp

---
+++ 线性表/顺序实现/P19_1.cpp
@@ -0,0 +1,19 @@
+//
+// 删除顺序表中最小的元素，空出的位置用最后一个元素代替
+//
+
+bool Del_Min(SqList& L)
+{
+	if(L.length == 0)   // 如果顺序表为空，返回false
+		return false;
+	int index = 0;      // 最小元素的数组下标，初始化为0
+	
+	// 找到最小元素的数组下标
+	for(int i = 1; i < L.length; i++)
+		if(L.data[i] < L.data[index])
+			index = i;
+	
+	// 用末尾元素覆盖最小元素，并将顺序表长度减1
+	L.data[index] = L.data[L.length - 1];
+	L.length--;
+}
