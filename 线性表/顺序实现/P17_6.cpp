896919e4cb13e6bd2555256656f114d60bd3b315
penu.c <87123013+penuins@users.noreply.github.com>
2021-10-09 23:23:22 +0800

    Update P17_6.cpp

--- 线性表/顺序实现/P17_6.cpp
+++ 线性表/顺序实现/P17_6.cpp
@@ -4,4 +4,21 @@
 
 void Del_Rep(SqList& L)
 {
+    // 双指针法
+    int slow = fast = 0;
+    
+    while(fast < L.length)
+    {
+        // 若当前元素不重复，则加入到新顺序表中
+        if(L.data[fast + 1] != L.data[slow])
+            L.data[++slow] = L.data[++fast];
+        // 否则处理下一个元素
+        else
+            ++fast;
+    }
+    
+    // 更新顺序表表长
+    L.length = slow + 1;
 }
+
+
