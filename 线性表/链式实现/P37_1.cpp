3d09cef3b69088098a96eb6ed4fa443056541653
penu.c <87123013+penuins@users.noreply.github.com>
2021-10-15 21:45:23 +0800

    Create P37_1.cpp

---
+++ 线性表/链式实现/P37_1.cpp
@@ -0,0 +1,20 @@
+//
+// 设计一个递归算法，删除不带头结点的单链表L中所有值为x的结点
+//
+
+void Del_x(LinkList& L, ElemType x)
+{
+    if(L == NULL)      // 边界条件
+        return;
+    
+    if(L.data == x)    // 当前元素等于x
+    {
+        LNode* p = L;
+        L=L->next;
+        p->next = NULL;
+        free(p);
+        Del_x(L, x);
+    }
+    else               // 当前元素不等于x
+        Del_x(L->next, x);
+}
